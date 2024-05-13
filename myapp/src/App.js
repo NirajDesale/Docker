7a6db41ef45ba158ba3de3e0cc72638bd151fb76
Paras Khonde <pkhonde@weltman.com>
2024-05-13 19:12:51 +0530

    app.js modified

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React  mypp vhgvh
+          Learn React with Docker image created in Azure
         </a>
       </header>
     </div>
